9a3763f9d38d99e0a7a14bc2abccb3bc6fd5113b
ikrambenallali <ikrambenallali2003@gmail.com>
2025-11-11 15:28:41 +0100

    add token dans axios

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'http://localhost:3000'
 });
-
+// Un intercepteur = une fonction qui s’exécute avant que la requête ne parte au backend
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
